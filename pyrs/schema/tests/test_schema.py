b054b16ee8774327d1be76f7d2843afd65adc63e
Csaba Palankai <csaba.palankai@gmail.com>
2015-08-13 00:11:14 +0100

    Add more tests

--- pyrs/schema/tests/test_schema.py
+++ pyrs/schema/tests/test_schema.py
@@ -32,3 +32,28 @@ class TestSchema(unittest.TestCase):
 
         t = MyObject()
         t.validate({"String": "value"})
+
+    def test_subschema(self):
+        class MySubObject(schema.Schema):
+            integer = types.Integer(name="int")
+
+        class MyObject(schema.Schema):
+            string = types.String(name="String")
+            sub = MySubObject(null=True)
+        t = MyObject()
+
+        self.assertEqual(
+            t.get_schema(),
+            {
+                "type": "object",
+                "properties": {
+                    "String": {"type": "string"},
+                    "sub": {
+                        "type": ["object", "null"],
+                        "properties": {"int": {"type": "integer"}}
+                    },
+                }
+            }
+        )
+        t.validate({"String": "hello", "sub": {"int": 12}})
+        t.validate({"String": "hello", "sub": None})
