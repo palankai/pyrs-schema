a21134d395369748827e2e0d0a02814ccbec65ed
Csaba Palankai <csaba.palankai@gmail.com>
2015-08-19 23:38:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email='csaba.palankai@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    version='0.3.1',
+    version='0.4',
     description="Python microservice framework",
     long_description=read('README.rst'),
     classifiers=[
